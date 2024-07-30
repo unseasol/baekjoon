a5296e9a713336aa5468e18209ddc15418b1fe0f
Jannys <slalstkd45@gmail.com>
2024-07-30 13:05:27 +0900

    finish snowball

--- Q17950_snowball.py
+++ Q17950_snowball.py
@@ -1,16 +1,31 @@
 # Question URL : https://www.acmicpc.net/problem/17950
 
+# import sys
+
+# H, x = map(int, sys.stdin.readline().split())
+
+# total = 0
+# r = 10**9+7
+
+# for i in range(1, H+1):
+#     snow_nums = int(sys.stdin.readline())
+#     total += (x**i*snow_nums) % r
+
+# print(total)
+
 import sys
 
 H, x = map(int, sys.stdin.readline().split())
 
 total = 0
-snowball = []
 r = 10**9+7
+temp = 1
 
-for i in range(1, H+1):
+for _ in range(H):
     snow_nums = int(sys.stdin.readline())
-    snowball.append(x**i*snow_nums)
+    temp *= x
+    temp = temp % r
+    total += snow_nums*temp
 
-total = sum(snowball) % r
+total = total % r
 print(total)
